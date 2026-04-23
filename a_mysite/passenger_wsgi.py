26aefd17844c6f7cad0e1103d8c2f58988d2cc79
denidatanalis-max <denidatanalis@gmail.com>
2026-04-23 16:34:38 +0700

    04232026

---
+++ a_mysite/passenger_wsgi.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'a_mysite.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
