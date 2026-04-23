00c8ab3af4e1901c2644075336f7a9e26092f0f7
denitmpgith <denimytmp@gmail.com>
2026-04-23 22:50:26 +0700

    105023042026

--- a_mysite/passenger_wsgi.py
+++ a_mysite/passenger_wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 try:
-    BASE_DIR = '/home/unitestm/depth'
+    BASE_DIR = '/home/unitestm/django-app'
     sys.path.insert(0, BASE_DIR)
 
     os.environ['DJANGO_SETTINGS_MODULE'] = 'a_mysite.settings'
